1e3f403e214442a06686bfb3a27f4de61cd85bab
Sofilove3 <saap1509@gmail.com>
2025-11-23 09:45:17 +0100

    LED turns on and off

--- LEDturnon/src/main.c
+++ LEDturnon/src/main.c
@@ -1,1 +1,17 @@
-void app_main() {}
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main() {
+    int LEDpin = 2;
+    gpio_set_direction(LEDpin, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        gpio_set_level(LEDpin, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(LEDpin, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    
+}
